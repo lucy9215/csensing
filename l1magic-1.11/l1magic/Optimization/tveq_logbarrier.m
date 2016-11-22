de921c314c14fe01a0be571592f2f72e81992153
lucy9215 <lucy9215@qq.com>
2016-11-22 19:23:00 +0800

    read tv optimization

--- l1magic-1.11/l1magic/Optimization/tveq_logbarrier.m
+++ l1magic-1.11/l1magic/Optimization/tveq_logbarrier.m
@@ -33,7 +33,7 @@
 %      Default = 10.   迭代的步长
 %
 % slqtol - Tolerance for SYMMLQ; ignored if A is a matrix.
-%     Default = 1e-8.   可忍受的误差 如果是矩阵则忽略此参数
+%     Default = 1e-8.   可忍受的误差 如果是矩阵则忽略此参数，因为矩阵不是largescal
 %
 % slqmaxiter - Maximum number of iterations for SYMMLQ; ignored
 %     if A is a matrix.
@@ -63,6 +63,13 @@ n = round(sqrt(N));   % 通过N计算出n
 
 % create (sparse) differencing matrices for TV
 % spdiags 取出非零元素
+% [-ones(n-1,n); zeros(1,n)]是n*n的矩阵，n-1*n为-1，最后一行为0
+% reshape以后按列取，变成n-1个-1，然后一个0重复...的一维向量
+% [reshape([-ones(n-1,n); zeros(1,n)],N,1) ...
+%  reshape([zeros(1,n); ones(n-1,n)],N,1)]就成了两行向量
+% 经过spdiags函数以后，Dv就成了对角线是-1从(1,1)-(63,63)，对角线左上的斜线是1的稀疏矩阵(1,2)(63,64)
+% Dh是对角线为-1 距离为16的地方(1,17)开始是1的稀疏矩阵
+
 Dv = spdiags([reshape([-ones(n-1,n); zeros(1,n)],N,1) ...
   reshape([zeros(1,n); ones(n-1,n)],N,1)], [0 1], N, N);
 Dh = spdiags([reshape([-ones(n,n-1) zeros(n,1)],N,1) ...
@@ -70,18 +77,18 @@ Dh = spdiags([reshape([-ones(n,n-1) zeros(n,1)],N,1) ...
 
 % starting point --- make sure that it is feasible
 if (largescale)
-  if (norm(A(x0)-b)/norm(b) > slqtol)
+  if (norm(A(x0)-b)/norm(b) > slqtol) % 对传入的x0投影，减去观测值得到残差，求残差和观测值的2范数比例 大于可忍受的
     disp('Starting point infeasible; using x0 = At*inv(AAt)*y.');
     AAt = @(z) A(At(z));
-    [w,cgres] = cgsolve(AAt, b, slqtol, slqmaxiter, 0);
-    if (cgres > 1/2)
+    [w,cgres] = cgsolve(AAt, b, slqtol, slqmaxiter, 0); % 这里用到了函数cgsolve
+    if (cgres > 1/2)                   % 检查是否满足求解条件
       disp('A*At is ill-conditioned: cannot find starting point');
       xp = x0;
       return;
     end
-    x0 = At(w);
+    x0 = At(w); % 求解后的w反投影为x0
   end
-else
+else    %　小规模下是否满足求解条件
   if (norm(A*x0-b)/norm(b) > slqtol)
     disp('Starting point infeasible; using x0 = At*inv(AAt)*y.');
     opts.POSDEF = true; opts.SYM = true;
@@ -91,33 +98,33 @@ else
       xp = x0;
       return;
     end
-    x0 = A'*w;
+    x0 = A'*w; % 对解出的w反投影为x0
   end  
 end
-x = x0;
-Dhx = Dh*x;  Dvx = Dv*x;
-t = (0.95)*sqrt(Dhx.^2 + Dvx.^2) + (0.1)*max(sqrt(Dhx.^2 + Dvx.^2));
+x = x0;   % 为x赋初始值
+Dhx = Dh*x;  Dvx = Dv*x; %　求gradient　horizontal and vertical
+t = (0.95)*sqrt(Dhx.^2 + Dvx.^2) + (0.1)*max(sqrt(Dhx.^2 + Dvx.^2)); % 加权方法算出来的gradient值
 
-% choose initial value of tau so that the duality gap after the first
+% choose initial value of tau so that the duality(二元) gap after the first
 % step will be about the origial TV
-tau = N/sum(sqrt(Dhx.^2+Dvx.^2));
+tau = N/sum(sqrt(Dhx.^2+Dvx.^2));   % 这个tau不知道是什么
 
-lbiter = ceil((log(N)-log(lbtol)-log(tau))/log(mu));
+lbiter = ceil((log(N)-log(lbtol)-log(tau))/log(mu));   %计算logbarrier的迭代次数
 disp(sprintf('Number of log barrier iterations = %d\n', lbiter));
 totaliter = 0;
-for ii = 1:lbiter
+for ii = 1:lbiter  % 进行logbarrier迭代
   
-  [xp, tp, ntiter] = tveq_newton(x, t, A, At, b, tau, newtontol, newtonmaxiter, slqtol, slqmaxiter);
-  totaliter = totaliter + ntiter;
+  [xp, tp, ntiter] = tveq_newton(x, t, A, At, b, tau, newtontol, newtonmaxiter, slqtol, slqmaxiter); % 进行迭代 返回值中有迭代次数
+  totaliter = totaliter + ntiter;  %总迭代次数由牛顿迭代的次数累加
   
-  tvxp = sum(sqrt((Dh*xp).^2 + (Dv*xp).^2));
+  tvxp = sum(sqrt((Dh*xp).^2 + (Dv*xp).^2)); % 总的gradient值 
   disp(sprintf('\nLog barrier iter = %d, TV = %.3f, functional = %8.3f, tau = %8.3e, total newton iter = %d\n', ...
-    ii, tvxp, sum(tp), tau, totaliter));
+    ii, tvxp, sum(tp), tau, totaliter));  % 这里的functional不知道是什么
   
   x = xp;
   t = tp;
   
-  tau = mu*tau;
+  tau = mu*tau; %　新的tau是步长mu*tau
   
 end
                    
--- testMatrix.m
+++ testMatrix.m
@@ -1,20 +1,37 @@
 clear all
-M=8;
-N=12;
-Phi=PartHadamardMtx(M,N);
+% M=8;
+% N=12;
+% Phi=PartHadamardMtx(M,N);
+% 
+% 
+% 
+% A=rgb2gray(imread('lena.jpg'));
+% figure;
+% imshow(A);
+% 
+% F=fft2(A);
+% Fim=log10(abs(F));
+% figure;
+% imshow(Fim,[]);
+% 
+% S=fftshift(abs(F));
+% S_norm=log10(S);
+% figure;
+% imshow(S_norm,[]);
+
+n=16;
+N=n*n;
+x=[-ones(n-1,n); zeros(1,n)];
+xl=reshape([-ones(n-1,n); zeros(1,n)],N,1);
+Dv=[reshape([-ones(n-1,n); zeros(1,n)],N,1) ...
+  reshape([zeros(1,n); ones(n-1,n)],N,1)];
+
+Dvv=spdiags([reshape([-ones(n-1,n); zeros(1,n)],N,1) ...
+  reshape([zeros(1,n); ones(n-1,n)],N,1)], [0 1], N, N);
+
+Dh = spdiags([reshape([-ones(n,n-1) zeros(n,1)],N,1) ...
+  reshape([zeros(n,1) ones(n,n-1)],N,1)], [0 n], N, N);
 
 
 
-A=rgb2gray(imread('lena.jpg'));
-figure;
-imshow(A);
 
-F=fft2(A);
-Fim=log10(abs(F));
-figure;
-imshow(Fim,[]);
-
-S=fftshift(abs(F));
-S_norm=log10(S);
-figure;
-imshow(S_norm,[]);
